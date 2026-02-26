32d2d65eced37f3eca7a28087e91c15f64d137ee
nicoletabernero5112-lgtm <nicole.tabernero5112@gmail.com>
2026-02-26 15:06:18 -0500

    exercise updates

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import DurationExercise from "./components/DurationExercise";
 import RepetitionExercise from "./components/RepetitionExercise";
 
 function App() {
-  let [exerciseType, setExerciseType] = useState("null");
+  let [exerciseChoice, setExerciseChoice] = useState("null");
   let exercises = [
     { exerciseName: "Push Ups", exerciseType: "repetition" },
     { exerciseName: "Plank", exerciseType: "duration" },
     { exerciseName: "Running", exerciseType: "duration" },
   ];
+  if (exerciseChoice) {
+    if (exerciseChoice.exerciseType === "repetition") {
+      return <RepetitionExercise />;
+    } else {
+      return <DurationExercise />;
+    }
+  }
 
+  return (
   <div>
     <h1>Exercise Mania</h1>
     <p>Select one exercise!</p>
     <div>
     {exercises.map((exercise) => (
       <button key={exercise.exerciseName} 
-      onClick={() => setExerciseType(exercise.exerciseType)}>
+      onClick={() => setExerciseChoice(exercise.exerciseName)}>
         {exercise.exerciseName}
       </button>
     ))}
     </div>
   </div>
+  )
 }
 
 export default App;
